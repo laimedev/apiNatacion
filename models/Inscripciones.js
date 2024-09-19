71595902fee3ba73eb96580b49f9b54fe1a71f37
AlexLaime <alaimes64@gmail.com>
2024-09-19 04:00:52 -0500

    dfdsf

--- models/Inscripciones.js
+++ models/Inscripciones.js
@@ -14,45 +14,46 @@ const Inscripciones = {
   },
 
   // Función para obtener las inscripciones y pagos por codCliente
-  getByClienteWithPayments: async (codCliente) => {
-    try {
-      const connection = await dbConnection();
-      const query = `
-        SELECT 
-          i.codInscripcion,
-          i.fechaInscripcion,
-          i.costoTarifa,
-          i.codAlumno,
-          i.codCliente,
-          i.codHorario,
-          i.tiempo,
-          i.clases,
-          i.email AS emailInscripcion,
-          p.fechaPago,
-          p.metodoPago,
-          p.importePago,
-          p.venta_id,
-          c.nombres,
-          c.primer_apellido,
-          c.segundo_apellido,
-          c.telefono,
-          c.email AS emailCliente
-        FROM 
-          Inscripciones i
-        LEFT JOIN 
-          Pagos p ON i.codInscripcion = p.codInscripcion
-        LEFT JOIN 
-          Clientes c ON i.codCliente = c.codCliente
-        WHERE 
-          i.codCliente = ?`;
-      
-      const [rows] = await connection.query(query, [codCliente]);
-      connection.release();
-      return rows;
-    } catch (error) {
-      throw new Error('Error al obtener inscripciones con pagos y datos del cliente: ' + error);
+    getByClienteWithPayments: async (codCliente) => {
+      try {
+        const connection = await dbConnection();
+        const query = `
+          SELECT 
+            i.codInscripcion,
+            i.fechaInscripcion,
+            i.costoTarifa,
+            i.codAlumno,
+            i.codCliente,
+            i.codHorario,
+            i.tiempo,
+            i.clases,
+            i.email AS emailInscripcion,
+            p.fechaPago,
+            p.metodoPago,
+            p.importePago,
+            p.venta_id,
+            c.nombres,
+            c.primer_apellido,
+            c.segundo_apellido,
+            c.telefono,
+            c.email AS emailCliente,
+            c.numDocumento
+          FROM 
+            Inscripciones i
+          LEFT JOIN 
+            Pagos p ON i.codInscripcion = p.codInscripcion
+          LEFT JOIN 
+            Clientes c ON i.codCliente = c.codCliente
+          WHERE 
+            i.codCliente = ?`;
+        
+        const [rows] = await connection.query(query, [codCliente]);
+        connection.release();
+        return rows;
+      } catch (error) {
+        throw new Error('Error al obtener inscripciones con pagos y datos del cliente: ' + error);
+      }
     }
-  }
   
 };
 
